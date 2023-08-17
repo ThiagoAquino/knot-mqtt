d7d21dc671ca32006e0b5492c973b438e6d09ff7
Thiago <tas3@cesar.org.br>
2023-08-17 08:42:03 -0300

    Fix methodo to unsmarshall json
    Signed-off-by: Thiago <tas3@cesar.org.br>

--- pkg/application/mqtt_consumer.go
+++ pkg/application/mqtt_consumer.go
@@ -6,8 +6,10 @@ import (
 	"github.com/CESARBR/knot-mqtt/internal/entities"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"log"
+	"math"
 	"os"
 	"os/signal"
+	"strconv"
 )
 
 func ConfigureClient(mqttConfiguration entities.MqttConfig) mqtt.Client {
@@ -145,92 +147,46 @@ func VerifyError(err error) {
 //	}
 func onMessageReceived(msg mqtt.Message, transmissionChannel chan entities.CapturedData, deviceConfiguration map[string]entities.Device, mqttDeviceConfiguration entities.DeviceConfig) {
 	var data map[string]interface{}
-	var capturedData struct {
-		ID   int                      `json:"id"`
-		Data []map[string]interface{} `json:"data"`
-	}
-
 	err := json.Unmarshal([]byte(msg.Payload()), &data)
 	fmt.Println(data)
 	if err != nil {
 		fmt.Println("Erro ao converter JSON:", err)
 		return
 	}
-
 	for _, config := range mqttDeviceConfiguration.Config {
 		var finalData entities.CapturedData
-		value, _ := data[config.Value]
-		name, _ := data[config.Name]
-		times, _ := data[config.Timestamp]
-		sensorType, _ := data[config.SensorType]
-
+		value, _ := data[config.Value].(string)
+		name, _ := data[config.Name].(string)
+		times, _ := data[config.Timestamp].(string)
+		sensorType, _ := data[config.SensorType].(float64)
 		fmt.Println("Value:", value)
 		fmt.Println("Name:", name)
 		fmt.Println("times:", times)
 		fmt.Println("sensorType:", sensorType)
-
-		for _ = range capturedData.Data {
-			var dataRow entities.Row
-			dataRow.Value = value
-			dataRow.Timestamp = times.(string)
-			dataRow.ID = sensorType.(int)
-
-			finalData.Rows = append(finalData.Rows, dataRow)
+		var dataRow entities.Row
+		dataRow.Value, _ = strconv.ParseFloat(value, 64)
+		dataRow.Timestamp = times
+		dataRow.ID = int(math.Round(sensorType))
+		finalData.ID = int(math.Round(sensorType))
+		finalData.Rows = append(finalData.Rows, dataRow)
+		// Imprimir os dados decodificados
+		for _, row := range finalData.Rows {
+			fmt.Println("SensorId:", finalData.ID)
+			fmt.Println("Value:", row.Value)
+			fmt.Println("Timestamp:", row.Timestamp)
 		}
 		transmissionChannel <- finalData
 	}
 
-	//// Construir a estrutura final removendo os campos indesejados
-	//var finalData entities.CapturedData
-	//dataId := capturedData.ID
-	//for _, dataMap := range capturedData.Data {
-	//	var dataRow entities.Row
-	//
-	//	sensorId, _ := dataMap["sensorId"].(float64)
-	//	finalData.ID = int(sensorId)
-	//
-	//	switch deviceType {
-	//
-	//	case "int":
-	//		value, _ := dataMap["value"].(float64)
-	//		dataRow.Value = int(value)
-	//	case "float":
-	//		value, _ := dataMap["value"].(float64)
-	//		dataRow.Value = value
-	//	case "bool":
-	//		value, _ := dataMap["value"].(bool)
-	//		dataRow.Value = value
-	//	case "raw":
-	//		value, _ := dataMap["value"].(string)
-	//		dataRow.Value = value
-	//	case "int64":
-	//		value, _ := dataMap["value"].(float64)
-	//		dataRow.Value = int64(value)
-	//	case "uint":
-	//		value, _ := dataMap["value"].(float64)
-	//		dataRow.Value = uint(value)
-	//	case "double":
-	//		value, _ := dataMap["value"].(float64)
-	//		dataRow.Value = value
-	//	default:
-	//		fmt.Println("Erro no id")
-	//	}
-	//
-	//	timestamp, _ := dataMap["timestamp"].(string)
-	//	dataRow.Timestamp = timestamp
-	//
-	//	// Outros campos podem ser tratados da mesma maneira
-	//
-	//	finalData.Rows = append(finalData.Rows, dataRow)
-	//}
-	//
-	//// Imprimir os dados decodificados
-	//fmt.Println("CapturedData:", dataId)
-	//for _, row := range finalData.Rows {
-	//	fmt.Println("SensorId:", finalData.ID)
-	//	fmt.Println("Value:", row.Value)
-	//	fmt.Println("Timestamp:", row.Timestamp)
-	//}
-	//
-
 }
+
+//type T struct {
+//	Id   string `json:"id"`
+//	Data []struct {
+//		SensorId  int    `json:"sensorId"`
+//		Value     string `json:"value"`
+//		Timestamp string `json:"timestamp"`
+//	} `json:"data"`
+//}
+//
+//{"id":"0d7cd9d221395e1e","data":[{"sensorId":1,"value":"12","timestamp":"2023-08-17 08:06:04"}]}
